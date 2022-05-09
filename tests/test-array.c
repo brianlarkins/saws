fab7bbe888806fb4d910e157a97ebef20f28fca3
Grace Hill <hilga-22@rhodes.edu>
2022-05-09 10:05:51 -0500

    trying and failing to create an array for owner processes

--- tests/test-array.c
+++ tests/test-array.c
@@ -10,22 +10,46 @@
 #define MAXHOSTNAME 64
 
 char host_arr[MAXHOSTS][MAXHOSTNAME];
+int owner_arr[MAXHOSTS];
 
 int main (int argc, char *argv[]) {
   char hostbuffer[BUFFER_SIZE];
 
   gtc_init();
 
-  //char dest[nprocs];
+  
 
   gethostname(hostbuffer, sizeof(hostbuffer));
-  shmem_putmem(host_arr[_c->rank],hostbuffer,MAXHOSTNAME, 0); 
+  shmem_putmem(host_arr[_c->rank],hostbuffer,MAXHOSTNAME, 0);
+  
+/*  for (int i=0; i <=_c->size; i+=48){
+    if (_c->rank >= i && _c->rank < i+48) {
+      //shmem_putmem(owner_arr[_c->rank], i, sizeof(i), 0)
+      owner_arr[_c->rank] = i;
+      break;
+    }
+  }
+*/
+
+
   shmem_barrier_all();
+
+
+  /*for (int k=0; k<_c->size; k++) {
+    if (host_arr[k] == host_arr[_c->rank]) {
+      owner_arr[_c->rank] = k;
+      break;
+    }
+  }*/
 
   if (_c->rank == 0) {
     for (int i=0; i<_c->size; i++) {
       printf("hostname for rank %d is %s\n", i, host_arr[i]);
     }
+
+   /* for (int j=0; j<_c->size;j++) {
+      printf("owner process for rank %d is %d\n", j, owner_arr[j]);
+    }*/
   }
 
   gtc_fini();
